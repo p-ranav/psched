145cede91748bd043f789effba2d135d65364239
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-07-30 15:49:00 -0500

    Updated multiple periodic tasks sample for readability

--- include/psched/priority_scheduler.h
+++ include/psched/priority_scheduler.h
@@ -17,6 +17,8 @@ template <size_t P> struct priority_levels { constexpr static size_t value = P; 
 
 template <size_t P> struct priority { constexpr static size_t value = P; };
 
+template <class D, size_t P> struct period { constexpr static D value = D(P); };
+
 template <class threads, class priority_levels> class PriorityScheduler {
   std::vector<std::thread> threads_;
   std::array<TaskQueue, priority_levels::value> priority_queues_;
@@ -93,6 +95,19 @@ public:
       enqueued_ = true;
       ready_.notify_one();
     }
+  }
+
+  template <class priority, class period>
+  void schedule(Task task) {
+    std::thread([this, &task]() {
+      do {
+        // schedule task at priority level 2
+        schedule<priority>(task);
+
+        // sleep for 100ms
+        std::this_thread::sleep_for(period::value);
+      } while (true);
+    }).detach();
   }
 
   void stop() {
--- samples/single_periodic_task.cpp
+++ samples/single_periodic_task.cpp
@@ -8,29 +8,21 @@ int main() {
   PriorityScheduler<threads<2>, priority_levels<3>> scheduler;
 
   // Configure task
-  Task t;
-  t.on_execute([] {
+  Task task;
+
+  task.on_execute([] {
     // execution time (burst time) of task = 40ms
     std::this_thread::sleep_for(std::chrono::milliseconds(40));
   });
 
-  t.on_complete([](const TaskStats& stats) {
+  task.on_complete([](const TaskStats& stats) {
     std::cout << "Timer 1 fired! ";
     std::cout << "Waiting time = " << stats.waiting_time() << "ms; ";
     std::cout << "Burst time = " << stats.burst_time() << "ms; ";
     std::cout << "Turnaround time = " << stats.turnaround_time() << "ms\n";
   });
 
-  // Schedule task periodically
-  auto timer1 = std::thread([&scheduler, &t]() {
-    do {
-      // schedule task at priority level 2
-      scheduler.schedule<priority<2>>(t);
-
-      // sleep for 100ms
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    } while (true);
-  });
-
-  timer1.join();
+  // Schedule periodic task
+  scheduler.schedule<priority<2>, 
+                     period<std::chrono::milliseconds, 100>>(task);
 }
